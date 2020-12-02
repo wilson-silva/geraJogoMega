1af2eb369ee9989d4ae3b7a781f7dc43a233394b
Wilson <wilson-gs@hotmail.com>
2020-12-01 21:42:04 -0300

    comentado trecho de código sem utilidade no momento

--- core/views.py
+++ core/views.py
@@ -13,20 +13,21 @@ def home(request):
         n = int(request.POST['variavel'])
         for j in range(n):
             cont = 0
-            while cont < 15:
-                num = randint(1, 26)
+            while cont < 6:
+                num = randint(1, 61)
                 if num not in jogo:
                     jogo.append(num)
                     cont += 1
             jogos.append(jogo[:])
             jogo.clear()
 
+    """ 
         for i in range(1, 26):
             count = 0
             for j in jogos:
                 if i in j:
                     count += 1
             repeticao.append(count)
-
+    """
     contexto = {'jogos': jogos, 'repeticao': repeticao}
     return render(request, 'index.html', contexto)
